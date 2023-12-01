5a500b361d5ea2fb530b23acca498cd392fe30c9
Daniel Ferreira <daniel.ferreira@eduzz.com>
2023-12-01 10:56:10 -0300

    dashboard project nextjs

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DashboardSkeleton from '../../ui/skeletons'
+
+interface Props {}
+
+const Loading = () => {
+  return <div><DashboardSkeleton /></div>
+}
+
+export default Loading
